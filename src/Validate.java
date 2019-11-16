4b328dd9170064ded60f7260c8185c040b6d4725
christjankinasch <c.kinasch@gmail.com>
2019-11-16 12:36:00 +1100

    more todos, cleaned Config, begun Validation boilerplate

--- src/Validate.java
+++ src/Validate.java
@@ -1,2 +1,37 @@
 public class Validate {
+    /**
+     * Input validation functions (regex operations?)
+     * - Ports
+     * - IPs
+     * - String cleaning
+     * - Valid Keys format (2066)
+     */
+
+    public boolean isValidIp(String args) {
+        boolean isValid = false;
+        String splitString;
+        int[] parsedInts;
+        int[] moreThan4Items;
+        int[] max255;
+
+        return isValid;
+    }
+
+    public boolean isValidPort(String args) {
+        boolean isValid = false;
+
+        return isValid;
+    }
+
+    public boolean isCleanString(String args) {
+        boolean isValid = false;
+
+        return isValid;
+    }
+
+    public boolean isValidKeyFormat(String args) {
+        boolean isValid = false;
+
+        return isValid;
+    }
 }
